58452cb8d6de3a6775a2d2f087ef9e3376b6bdb1
Antonio Marcos <marcosgeo@yahoo.com>
2020-05-23 17:52:23 -0300

    basic mvc structure finished.

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -1,8 +1,10 @@
 package domain
 
 import (
-	"errors"
 	"fmt"
+	"net/http"
+
+	"github.com/marcosgeo/microservices/mvc/utils"
 )
 
 var (
@@ -12,9 +14,13 @@ var (
 )
 
 // GetUser ...
-func GetUser(userID int64) (*User, error) {
+func GetUser(userID int64) (*User, *utils.ApplicationError) {
 	if user := users[userID]; user != nil {
 		return user, nil
 	}
-	return nil, errors.New(fmt.Sprintf("User %v was not found", userID))
+	return nil, &utils.ApplicationError{
+		Message:    fmt.Sprintf("User %v was not found", userID),
+		StatusCode: http.StatusNotFound,
+		Code:       "not_found",
+	}
 }
