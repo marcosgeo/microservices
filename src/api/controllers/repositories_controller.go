906931c52a238bdfdab915e6a7adbc6a9930220c
Antonio Marcos <marcosgeo@yahoo.com>
2020-06-25 18:46:59 -0300

    controller layer created with CreateRepo function.

---
+++ src/api/controllers/repositories_controller.go
@@ -0,0 +1,27 @@
+package repositories
+
+import (
+	"net/http"
+
+	"github.com/gin-conic/gin"
+	"github.com/marcosgeo/microservices/src/api/domain/repositories"
+	"github.com/marcosgeo/microservices/src/api/services"
+	"github.com/marcosgeo/microservices/src/api/utils/errors"
+)
+
+// CreateRepo ...
+func CreateRepo(c *gin.Context) {
+	var request repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+
+	result, err := services.RepositoryService.CreateRepo(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
