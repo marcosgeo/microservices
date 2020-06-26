068e94cccbbd54ce5c5928d0c7895d102e4584e5
Antonio Marcos <marcosgeo@yahoo.com>
2020-06-26 20:12:35 -0300

    adding test for service.

---
+++ src/api/services/repositories_service_test.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/marcosgeo/microservices/src/api/clients/restclient"
+	"github.com/marcosgeo/microservices/src/api/domain/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	restclient.StartMockups()
+	os.Exit(m.Run())
+}
+
+func TestCreateRepo_InvalidInputName(t *testing.T) {
+	request := repositories.CreateRepoRequest{} // creates an empty request
+
+	result, err := RepositoryService.CreateRepo(request)
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "Invalid repository name", err.Message())
+}
+
+func TestCreateRepo_ErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires authentication", "documentation_url": "https://developer.github.com/docs"}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	result, err := RepositoryService.CreateRepo(request)
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "Requires authentication", err.Message())
+}
+
+func TestCreateRepo_NoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123, "name": "testing", "owner": {"login": "marcosgeo"}}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	result, err := RepositoryService.CreateRepo(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 123, result.ID)
+	assert.EqualValues(t, "testing", result.Name)
+	assert.EqualValues(t, "marcosgeo", result.Owner)
+}
